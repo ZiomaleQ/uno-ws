dbafd2aad0a5e8101184abb4e45a35851e79b631
ZiomaleQ <r.partyka30@gmail.com>
2022-02-28 20:24:18 +0100

    LOBBY MA BOIZ

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,9 +1,16 @@
 <script>
   const socket = io();
+  let userID = "";
+
+  socket.on("userInfo", (user) => {
+    userID = user.id;
+  });
 
   let room = {};
 
   let username = "Guest";
+  let roomID = "";
+  let spectate = false;
 
   socket.onAny(console.log);
 
@@ -11,8 +18,30 @@
     room = newRoom;
   });
 
+  socket.on("leaveRoom", () => {
+    room = {};
+    spectate = false;
+  });
+
+  socket.on("usersUpdate", (newUsers) => {
+    room.users = newUsers;
+  });
+
   function createRoom() {
     socket.emit("createRoom", username + "'s room");
+  }
+
+  function leaveRoom() {
+    socket.emit("leaveRoom");
+  }
+
+  function joinRoom() {
+    socket.emit("joinRoom", roomID);
+  }
+
+  function spectateRoom() {
+    socket.emit("spectateRoom", roomID);
+    spectate = true;
   }
 
   $: socket.emit("nameChange", username);
@@ -23,7 +52,31 @@
   <input type="text" bind:value={username} />
 
   {#if Object.keys(room).length === 0}
-    <button on:click={createRoom}> CREATE ROOM </button>
+    <button on:click={createRoom}>CREATE ROOM</button>
+
+    <br />
+
+    <input type="text" bind:value={roomID} />
+    <button on:click={joinRoom}>JOIN ROOM</button>
+    <button on:click={spectateRoom}>SPECTATE ROOM</button>
+  {:else}
+    <p>You're in '{room.name}' now.</p>
+    <p>Secret join code: <span>{room.id}</span></p>
+    <br />
+
+    <p>Users ({(room.users || []).length}):</p>
+
+    {#each room.users as { id, name, spectator }}
+      <p>
+        {name ? name : "Guest"}
+        {#if id === userID} (You) {/if}
+        {#if spectator || (id === userID && spectate)} (Ghost) {/if}
+      </p>
+    {:else}
+      {""}
+    {/each}
+
+    <button on:click={leaveRoom}>LEAVE ROOM</button>
   {/if}
 </main>
 
@@ -46,5 +99,13 @@
     main {
       max-width: none;
     }
+  }
+
+  span {
+    background-color: black;
+    color: white;
+    width: max-content;
+    height: max-content;
+    padding: 5px 10px;
   }
 </style>
--- index.js
+++ index.js
@@ -25,30 +25,92 @@ const rooms = []
 const users = {}
 
 io.on("connection", (socket) => {
+    socket.emit("userInfo", { id: socket.id })
     socket.on("createRoom", name => {
 
         const room = {
             id: random(16),
-            users: [socket],
+            users: [socket.id],
+            owner: socket.id,
+            spectators: [],
             name
         }
 
         rooms.push(room)
-
-        socket.emit("joinRoom", { id: room.id, name })
+        io.in(socket.id).socketsJoin(room.id)
+        socket.emit("joinRoom", { id: room.id, name, owner: room.owner, users: getRoomUsers(room.id) })
     })
+
     socket.on("joinRoom", (id) => {
         const index = rooms.findIndex(elt => elt.id === id)
         if (index === -1) {
-            socket.emit("UnoError", "Invalid ID")
+            return socket.emit("UnoError", "Invalid ID")
         }
+
+        const alreadyInRoom = rooms.some(elt => elt.users.includes(socket.id))
+
+        if (alreadyInRoom) {
+            return socket.emit("UnoError", "Already in room")
+        }
+
+        rooms[index].users.push(socket.id)
+        const { name, owner } = rooms[index]
+        io.in(socket.id).socketsJoin(id)
+        socket.emit("joinRoom", { id, name, owner, users: [] })
+        io.in(id).emit("usersUpdate", getRoomUsers(id))
+    })
+
+    socket.on("leaveRoom", () => {
+        const roomIndex = rooms.findIndex(elt => elt.users.includes(socket.id))
+
+        if (roomIndex === -1) {
+            return socket.emit("UnoError", "Not in room")
+        }
+        rooms[roomIndex].users.splice(rooms[roomIndex].users.findIndex(elt => elt === socket.id), 1)
+        io.in(socket.id).socketsLeave(rooms[roomIndex].id)
+        io.in(rooms[roomIndex].id).emit("usersUpdate", getRoomUsers(id))
+        socket.emit("leaveRoom")
+    })
+
+    socket.on("spectateRoom", id => {
+        const index = rooms.findIndex(elt => elt.id === id)
+        if (index === -1) {
+            return socket.emit("UnoError", "Invalid ID")
+        }
+
+        const alreadyInRoom = rooms.some(elt => elt.users.includes(socket.id))
+
+        if (alreadyInRoom) {
+            return socket.emit("UnoError", "Already in room")
+        }
+
+        rooms[index].spectators.push(socket.id)
+        const { name, owner } = rooms[index]
+        io.in(id).emit("usersUpdate", getRoomUsers(id))
+        io.in(socket.id).socketsJoin(id)
+        socket.emit("joinRoom", { id, name, owner, users: getRoomUsers(id) })
     })
 
     socket.on("nameChange", newName => {
         users[socket.id] = newName
-
-        console.log("SOMEBODY GOT A NEW NAME ", newName)
+        if (newName === "Guest") delete users[socket.id]
+        if (socket.rooms.size > 1) {
+            io.in([...socket.rooms].find(elt => elt !== socket.id))
+                .except(socket.id)
+                .emit("nameChange", { id: socket.id, name: newName })
+        }
     })
+
+    socket.onAny(console.log)
 })
 
 const random = (length = 8) => Math.random().toString(16).substring(2, length);
+function getRoomUsers(id) {
+    const room = rooms.find(elt => elt.id === id)
+    if (!room) return []
+
+    const normalUsers = room.users.map(id => { return { id, name: users[id], spectator: false } })
+    const spectators = room.spectators.map(id => { return { id, name: users[id], spectator: true } })
+
+    return [normalUsers, spectators].flat(1)
+}
